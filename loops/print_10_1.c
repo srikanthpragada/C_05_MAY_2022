cc3a4c81c1a1f21598c760e44adae7b4ccbec020
srikanthpragada <srikanthpragada@gmail.com>
2022-05-16 17:57:44 +0530

    16-MAY

---
+++ loops/print_10_1.c
@@ -0,0 +1,14 @@
+// print 10 to 1
+
+#include <stdio.h>
+
+void main()
+{
+  int n;
+
+      for(n = 10; n >= 1; n --)
+      {
+         printf("%d ", n);
+      }
+
+}
---
+++ loops/print_1_10.c
@@ -0,0 +1,14 @@
+// print 1 to 10
+
+#include <stdio.h>
+
+void main()
+{
+  int n;
+
+      for(n = 1; n <= 10; n ++)
+      {
+         printf("%d ", n);
+      }
+
+}
