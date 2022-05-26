1f0767fe903a9503193aebac48b1d41e852548fa
srikanthpragada <srikanthpragada@gmail.com>
2022-05-26 17:55:54 +0530

    26-MAY

--- arrays/search.c
+++ arrays/search.c
@@ -1,14 +1,24 @@
-// Password
+// Linear search in sorted array
 #include <stdio.h>
 
 void main()
 {
   int a[] = {4,6,8,10,20,30,40,44,55,66};
-  int i, sn = 25;
-
+  int i, found = 0, sn = 25;
 
       for(i=0; i < 10; i ++)
       {
+          if (a[i] == sn)
+          {
+              printf("%d", i);
+              found = 1;
+              break;
+          }
 
+          if(a[i] > sn)
+            break;
       }
+
+      if (!found)
+         printf("Not found!");
 }
